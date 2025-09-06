5f001399b3b88b9875b9d3cf5764539390469c28
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-09-06 09:00:11 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-apt upgrade -y
-apt install -y xfce4 xfce4-goodies tightvncserver xrdp dbus-x11
+cd
 
 # Neofetch
 wget -O /bin/neofetch https://raw.githubusercontent.com/dylanaraps/neofetch/refs/heads/master/neofetch
 chmod +x /bin/neofetch
 
 # auto startup
+sed -i '/rm -rf /tmp/.X*/d' .bashrc
+sed -i '/neofetch/d' .bashrc
+sed -i '/vncserver/d' .bashrc
 cat >> .bashrc << EOF
 rm -rf /tmp/.X*
 neofetch
@@ -15,8 +17,8 @@ vncserver
 EOF
 
 # VNC startup config
-mkdir -p ~/.vnc
-cat > ~/.vnc/xstartup << EOF
+mkdir -p .vnc
+cat > .vnc/xstartup << EOF
 #!/bin/sh
 
 unset SESSION_MANAGER
@@ -24,4 +26,8 @@ unset DBUS_SESSION_BUS_ADDRESS
 xrdb $HOME/.Xresources
 startxfce4 &
 EOF
-chmod +x ~/.vnc/xstartup
+chmod +x .vnc/xstartup
+
+# Install dependencies 
+apt upgrade -y
+apt install -y xfce4 xfce4-goodies tightvncserver xrdp dbus-x11
